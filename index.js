23800156cbeba74fd989964f2d3f57d7749e35db
Nicholas Stano <nicholasstano@gmail.com>
2019-02-27 00:32:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 // write your code below!
-var name = "Susan"
-var height = 39
-var message = `${name} is ${height} inches tall`
-https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/what-is-a-test#
-module.exports = {name, height, message}
